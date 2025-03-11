519155e4b533e3b65e0f75f7d671ecc9feec5077
Sumit Kumar <ceb.sumitkumar@gmail.com>
2025-03-11 19:59:55 +0530

    test message

--- src/test/java/testng/basicsoftestng/TestNGInvocation.java
+++ src/test/java/testng/basicsoftestng/TestNGInvocation.java
@@ -7,5 +7,6 @@ public class TestNGInvocation {
     @Test(invocationCount = 50, invocationTimeOut = 10)
     public void repeatingTestCaseExmple(){
         System.out.println("Repeating Test Case Example");
+        System.out.println("Good evening");
     }
 }
